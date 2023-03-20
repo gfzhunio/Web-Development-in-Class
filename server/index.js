f43337a8bc7b87f422749e90cc9fc06d3f6215dd
gfzhunio <90656820+gfzhunio@users.noreply.github.com>
2023-03-20 17:23:45 -0400

    add server, controllers, models, and scratch.http

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
-const http = require('http');
+const express = require('express')
+const products = require('./controllers/products')
+const app = express()
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello New Paltz. From Nodemon');
-});
+app
+  .get('/', (req, res) => {
+    res.send('Hello World From Express')
+})
 
-server.listen(port, () => {
+.use('products', products)
+
+app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
