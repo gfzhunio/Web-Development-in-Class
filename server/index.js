6188b984347453c01dce58b3eae9743a065a0da9
gfzhunio <90656820+gfzhunio@users.noreply.github.com>
2023-03-27 14:21:53 -0400

    building workspaces, deep linking and asy pro

--- server/index.js
+++ server/index.js
@@ -1,22 +1,36 @@
 const express = require('express')
-cons path = require('path')
+const path = require('path');
+const jokes = require('./controllers/jokes');
 const products = require('./controllers/products')
 const app = express()
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
+// Middleware
 app
-  .get('/', (req, res) => {
-    res.send('Hello World From Express')
-})
+    .use(express.json())
+    .use(express.static(path.join(__dirname, '../client/dist')))
 
-.use('/products', products)
 
-app.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// Actions
+app
+    .get('/api/v1/', (req, res) => {
+        res.send('Hello World! From Express')
+    })
+    .use('/api/v1/products', products)
+    .use('/api/v1/jokes', jokes)
 
+// Catch all
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
 })
+
+
+console.log('1: About to start server')
+
+app.listen(port, () => 
+  console.log(`2: Server running at http://${hostname}:${port}/`)
+);
+
+console.log('3: Asked server to start')
