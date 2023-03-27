a9906b8bf4a15cf52c65936b3a552f51a7c6427f
gfzhunio <90656820+gfzhunio@users.noreply.github.com>
2023-03-27 18:28:39 -0400

    session wrapper for api and fetch

--- client/src/model/session.ts
+++ client/src/model/session.ts
@@ -3,9 +3,15 @@
 
 import { reactive } from "vue";
 import { useRouter } from "vue-router";
+import * as myFetch from "./myFetch";
 
 const session = reactive({
     user: null as User | null,
+    isLoading: false,
+    messages: [] as {
+        msg: string,
+        type: "success" | "error" | "warning" | "info",
+    }[],
 })
 
 interface User {
@@ -20,9 +26,24 @@ export function useSession() {
     return session;
 }
 
+export function api(url: string) {
+    session.isLoading = true;
+    return myFetch.api(url)
+        .catch(err => {
+            console.error(err);
+            session.messages.push({
+                msg: err.message ?? JSON.stringify(err),
+                type: "error",
+            })
+        })
+        .finally(() => {
+            session.isLoading = false;
+        })
+}
+
 export function login() {
     session.user = {
-        name: "Gisela Fugon",
+        name: "John Doe",
     }
 }
 
