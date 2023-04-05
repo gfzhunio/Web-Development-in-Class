fd7b6cd7f3b1fbf9dda66c841eaa450311c63914
gfzhunio <90656820+gfzhunio@users.noreply.github.com>
2023-04-05 13:00:32 -0400

    post to server, admin UI,

--- client/src/model/products.ts
+++ client/src/model/products.ts
@@ -1,7 +1,7 @@
 /*  B"H
 */
 
-import type { DataListEnvelope } from "./myFetch";
+import type { DataEnvelope, DataListEnvelope } from "./myFetch";
 import { api } from "./session";
 
 export interface Product {
@@ -22,4 +22,14 @@ export function getProducts(): Promise<DataListEnvelope<Product>> {
 
     return api('products')
 
+}
+
+export function getProduct(id: number): Promise<DataEnvelope<Product>> {
+
+    return api(`products/${id}`)
+
+}
+
+export function createProduct(product: Product): Promise<DataEnvelope<Product>> {
+        return api('products', product)
 }
